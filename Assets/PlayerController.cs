4ee3b680c4ee99680efa5a90da409a44dbb3e1bc
eduarraiol7927 <edu.arraiol@gmail.com>
2026-03-13 23:51:33 -0300

    Tentativa de botar desaceleração

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -14,23 +14,30 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     //Atribuindo rigidbody pro rb
     }
-    public void OnMove(InputValue inputValue)
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+        public void OnMove(InputValue inputValue)
     //Usa o void porque ele não retorna nada. OnMove pra definir pra que serve. Os Parenteses
     // são pra receber o input que o jogador usar.
     {
         Vector2 input = inputValue.Get<Vector2>();
         //O vector2 input recebe a varíavel inputValue (ex: W, A, S, D), usando .Get<Vector2>() pra ser compreendido como um valor de x e y,
         //ou seja num caso de ter apertado W, x seria 0 e y seria 1.
-        rb.linearVelocity = input * Speed * Time.fixedDeltaTime;
-        Debug.Log(input);
-        Debug.Log(Speed);
+        rb.linearVelocity = input * Speed;
         //Atribuimos ao rb.linearVelocity (que é a velocidade do rigidbody, ou nosso personagem) 
         // o valor da tecla pressionada multiplicada pela velocidade resultando na movimentação do personagem.
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+if (inputValue.Get<Vector2>() != Vector2.zero)
+        {
+            Debug.Log("Jogador está se movendo.");
+        }
+        else{
+            
+        }
 
     }
 }
