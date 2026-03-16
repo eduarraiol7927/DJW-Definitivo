dd955ad4bf5ef4c5dc131988115948c26ac64c45
eduarraiol7927 <edu.arraiol@gmail.com>
2026-03-15 21:49:53 -0300

    15/03
    Movimentação e desaceleração ao parar concluídos

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,10 +3,12 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float Speed = 8f;
+    public float Speed = 10f;
     //Velocidade do personagem
     private Rigidbody2D rb;
-    //Abreviação do rigidbody pro codigo
+    //Variável que armazena o rigidbody
+    private Vector2 input;
+    //Pra lembrar o input do OnMove e usar no FixedUpdate
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,27 +19,41 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
     }
 
-        public void OnMove(InputValue inputValue)
-    //Usa o void porque ele não retorna nada. OnMove pra definir pra que serve. Os Parenteses
-    // são pra receber o input que o jogador usar.
+    void OnMove(InputValue inputValue)
+    //Usa o void porque ele não retorna nada. OnMove pra definir pra que serve. Os Parenteses são pra receber o input que o jogador
+    // usar.
     {
-        Vector2 input = inputValue.Get<Vector2>();
-        //O vector2 input recebe a varíavel inputValue (ex: W, A, S, D), usando .Get<Vector2>() pra ser compreendido como um valor de x e y,
-        //ou seja num caso de ter apertado W, x seria 0 e y seria 1.
-        rb.linearVelocity = input * Speed;
-        //Atribuimos ao rb.linearVelocity (que é a velocidade do rigidbody, ou nosso personagem) 
-        // o valor da tecla pressionada multiplicada pela velocidade resultando na movimentação do personagem.
+        input = inputValue.Get<Vector2>();
+        //O input recebe a varíavel inputValue (ex: W, A, S, D), e usamos o .Get<Vector2>() pra ser compreendido como um valor de x
+        // e y, ou seja num caso de ter apertado W, x seria 0 e y seria 1.
+    }
 
-if (inputValue.Get<Vector2>() != Vector2.zero)
+    void FixedUpdate()
+    //Usamos o FixedUpdate porque é melhor lidar com a física do jogo com ele.
+    {
+        if (input != Vector2.zero)
+        //Caso o input seja diferente de 0,
         {
             Debug.Log("Jogador está se movendo.");
+            rb.linearVelocity = input * Speed;
+        //Atribuimos ao rb.linearVelocity (que é a velocidade do RigidBody) o valor da tecla pressionada multiplicada pela
+        // velocidade resultando na movimentação do personagem.
         }
-        else{
-            
-        }
+        else
+        //Caso o input seja 0, então, a tecla foi solta,
+        {
+            Debug.Log("Jogador parou de se mover.");
+            rb.linearVelocity = new Vector2(Mathf.MoveTowards(rb.linearVelocity.x, 0f, 25f * Time.fixedDeltaTime), 
+            Mathf.MoveTowards(rb.linearVelocity.y, 0f, 25f * Time.fixedDeltaTime));
+            //Atribuimos ao rb.linearVelocity um novo valor/vector2, no entanto, com o Mathf.MoveTowards, que será responsável pela
+            // desaceleração, com a estrutura de seus parenteses (velocidade atual, velocidade alvo, aceleração multiplicada pelo 
+            // Time.fixedDeltaTime pro movimento ser consistente).
 
+            //Mais detalhadamente, o que acontece no Mathf.MoveTowards é que ele pega a velocidade atual e vai subtraindo a
+            // aceleração até chegar ao 0.
+        }
     }
 }
+
